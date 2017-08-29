f28d68b8f0f4ce9aef72ecd99825a0fdc409ebfe
anantakrroy <kr.innovate@gmail.com>
2017-08-29 12:00:42 +0530

    First commit

---
+++ helloJS/Javascript/learn-to-code.js
@@ -0,0 +1,40 @@
+//var name = "Jack";
+//var age = 23;
+//
+//var message = "Hi, my name is " + name + " and I am " + age + " years old";
+//console.log(message);
+
+//var age = 23;
+//var sum = 10 + 15;
+//var sub = 15-10;
+//var mult = 10*3;
+//var div = 9/3;
+//var remain = 10%4;
+//
+//
+//console.log(sum,sub,mult,div,remain);
+var accBalance = 300;
+var nikeSBShoes = 780.99;
+var coupon = 500;
+
+
+//if (nikeSBShoes <= accBalance) {
+//    accBalance -= nikeSBShoes;
+//    console.log("We just bought some dope shoes!" + " Your current account balance is " + accBalance);
+//} else if (nikeSBShoes -coupon <=accBalance) {
+//    console.log("We just bought some dope shoes with a coupon!");
+//    accBalance  -= nikeSBShoes - coupon;
+//    console.log("Account balance: "  + accBalance);
+//}
+//
+//else {
+//    console.log("You too broke for the shoes");
+//}
+var age = 23;
+var joesAge = "23";
+
+if (age !== joesAge) {   /* === is used  */
+    console.log("These are not the same");
+} else {
+    console.log("One is like the other");
+}
