39960e4212e40823b2a89a21e9019180e97de6de
anantakrroy <kr.innovate@gmail.com>
2017-08-30 23:31:17 +0530

    Changes made to learn-to-code.js/2nd commit

--- helloJS/Javascript/learn-to-code.js
+++ helloJS/Javascript/learn-to-code.js
@@ -30,11 +30,47 @@ var coupon = 500;
 //else {
 //    console.log("You too broke for the shoes");
 //}
-var age = 23;
-var joesAge = "23";
+//var age = 23;
+//var joesAge = "23";
+//
+//if (age !== joesAge) {   /* === is used  */
+//    console.log("These are not the same");
+//} else {
+//    console.log("One is like the other");
+//}
 
-if (age !== joesAge) {   /* === is used  */
-    console.log("These are not the same");
-} else {
-    console.log("One is like the other");
+/*FUNCTIONS*/
+
+var length1 = 15;
+var length2 = 19;
+var area1 = length1 * length2;
+console.log(area1);
+
+//Creating a function
+
+function area(length, width) {
+    return length * width;
 }
+
+console.log(area(10,32));
+
+var rectanglesAreas = [];
+rectanglesAreas.push(area(10,23));
+rectanglesAreas.push(area(10,2));
+rectanglesAreas.push(area(5,23));
+console.log("The area of the different rectangles are " + rectanglesAreas);
+
+var bankBalance = 500;
+function makeTransaction(priceOfSale) {
+    if (priceOfSale <= bankBalance) {
+        bankBalance -= priceOfSale;
+        console.log("Purchase Successful" +"\n" + "Remaining balance = " + bankBalance);
+    } else {
+        console.log("Insufficient Funds");
+    }
+}
+
+makeTransaction(79.00);
+makeTransaction(2.99);
+makeTransaction(10.99);
+makeTransaction(950);
