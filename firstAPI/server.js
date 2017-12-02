73cf6a0908d848059e83b546230ca0eb2bbb2a7e
anantakrroy <kr.innovate@gmail.com>
2017-12-02 21:16:12 +0530

    My First API

---
+++ firstAPI/server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var ingredients = [
+    {
+        "id" : "234fjj",
+        "text" : "Eggs"
+    },
+    {
+        "id" : "327fjg",
+        "text" : "Milk"
+    },
+    {
+        "id" : "996yhu",
+        "text" : "Cheese"
+    },
+    {
+        "id" : "449klo",
+        "text" : "Pancakes"
+    }
+];
+
+app.get('/', function(request, response){
+        
+        response.send(ingredients);
+        }
+    );
+
+app.post('/', function(request, response){
+   
+    var ingredient = request.body;
+    if (!ingredient || ingredient === "") {
+        response.status(500).send({error: "Your ingredient must be a text"});
+    } else {
+        ingredients.push(ingredient);
+        response.status(200).send (ingredients);
+    }
+});
+
+
+app.listen(3000, function() {
+    console.log('This API is running on port 3000');
+});
